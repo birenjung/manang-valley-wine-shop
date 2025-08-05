8d6746b8ccab2d101458b05975c422807a8579f5
birenjung <bjsampang@gmail.com>
2025-08-05 20:27:24 +0545

    made a layout for fe

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,62 +1,62 @@
- <div class="footer">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-4 col-md-6 mb-4 mb-lg-0">
-                    <h3 class="footer-heading">About Manang Valley</h3>
-                    <p>
-                        A brief, compelling description of your company, its mission, or what it offers. This can be a short mission statement or a summary of your services.
-                    </p>
-                    <div class="social-icons">
-                        <a href="#" class="fa fa-facebook"></a>
-                        <a href="#" class="fa fa-twitter"></a>
-                        <a href="#" class="fa fa-youtube"></a>
-                        <a href="#" class="fa fa-instagram"></a>
-                    </div>
-                </div>
-
-                <div class="col-lg-4 col-md-6 mb-4 mb-lg-0">
-                    <h3 class="footer-heading">Quick Links</h3>
-                    <ul class="list-unstyled">
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">Shop</a></li>
-                        <li><a href="#">About Us</a></li>
-                        <li><a href="#">Services</a></li>
-                        <li><a href="#">Contact</a></li>
-                    </ul>
-                </div>
-
-                <div class="col-lg-4 col-md-6 mb-4 mb-lg-0">
-                    <h3 class="footer-heading">Contact</h3>
-                    <ul class="list-unstyled">
-                        <li><span class="d-block">Address:</span> <span>[Your Company Address]</span></li>
-                        <li><span class="d-block">Phone:</span> <span>+977 98xxxxxxx</span></li>
-                        <li><span class="d-block">Email:</span> <span>[Your Company Email]</span></li>
-                    </ul>
+<div class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-6 mb-4 mb-lg-0">
+                <h3 class="footer-heading">About Manang Valley</h3>
+                <p>
+                    A brief, compelling description of your company, its mission, or what it offers.
+                </p>
+                <div class="social-icons">
+                    <a href="#" class="fa fa-facebook"></a>
+                    <a href="#" class="fa fa-twitter"></a>
+                    <a href="#" class="fa fa-youtube"></a>
+                    <a href="#" class="fa fa-instagram"></a>
                 </div>
             </div>
 
-            <div class="row pt-5">
-                <div class="col-12 text-center">
-                    <copyright style="text-align: center; padding: 20px; font-size: 14px; color: #555;">
-                        <p>
-                            &copy; 2025. All rights reserved for
-                            <a href="https://manangvalleywinehk.com" target="_blank" style="text-decoration: none; color: inherit;">
-                                manangvalleywinehk.com
-                            </a>.
-                        </p>
-                        <p>
-                            Template originally designed by
-                            <a href="https://colorlib.com" target="_blank" style="text-decoration: none; color: inherit;">
-                                Colorlib
-                            </a>.
-                            All customizations and backend development by
-                            <a href="https://www.birendrajungrai.com.np" target="_blank" style="text-decoration: none; color: inherit;">
-                                <strong>BirenJung Tech</strong> </a>
-                        </p>
-                    </copyright>
-
-                </div>
+            <div class="col-lg-4 col-md-6 mb-4 mb-lg-0">
+                <h3 class="footer-heading">Quick Links</h3>
+                <ul class="list-unstyled">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Shop</a></li>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
             </div>
 
+            <div class="col-lg-4 col-md-6 mb-4 mb-lg-0">
+                <h3 class="footer-heading">Contact</h3>
+                <ul class="list-unstyled">
+                    <li><span class="d-block">Address:</span> <span>[Your Company Address]</span></li>
+                    <li><span class="d-block">Phone:</span> <span>+977 98xxxxxxx</span></li>
+                    <li><span class="d-block">Email:</span> <span>[Your Company Email]</span></li>
+                </ul>
+            </div>
         </div>
+
+        <div class="row pt-5">
+            <div class="col-12 text-center">
+                <div class="copyright">
+                    <p>
+                        &copy; <script>document.write(new Date().getFullYear());</script> All rights reserved for
+                        <a href="https://manangvalleywinehk.com" target="_blank">
+                            manangvalleywinehk.com
+                        </a>.
+                    </p>
+                    <p>
+                        Template originally designed by
+                        <a href="https://colorlib.com" target="_blank">
+                            Colorlib
+                        </a>.
+                        All customizations and backend development by
+                        <a href="https://www.birendrajungrai.com.np" target="_blank">
+                            <strong>BirenJung Tech</strong>
+                        </a>
+                    </p>
+                </div>
+            </div>
+        </div>
+
     </div>
+</div>
