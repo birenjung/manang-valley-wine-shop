2ae9a7db39af7b3fc123c51afe740bae1b86e6e4
birenjung <bjsampang@gmail.com>
2025-08-05 19:36:56 +0545

    made a layout for fe

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Manang Valley Wine Shop</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    {{-- Google Fonts --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&family=Poppins:wght@400;700&display=swap" rel="stylesheet">
+
+    {{-- Third-Party CSS --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    
+    {{-- Project CSS --}}
+    <link rel="stylesheet" href="{{ asset('fe-assets/css/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('fe-assets/css/owl.theme.default.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('fe-assets/css/style.css') }}">
+    
+    {{-- Yield any page-specific CSS --}}
+    @yield('styles')
+</head>
+
+<body class="retro-neon-theme" data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+    <div class="site-wrap">
+        {{-- Include the header component --}}
+        @include('components.header')
+
+        {{-- The main content section --}}
+        @yield('content')
+
+        {{-- Include the footer component --}}
+        @include('components.footer')
+    </div>
+
+    {{-- Third-Party JS --}}
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+
+    {{-- Project JS --}}
+    <script src="{{ asset('fe-assets/js/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('fe-assets/js/main.js') }}"></script>
+
+    {{-- Yield any page-specific scripts --}}
+    @yield('scripts')
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,145 +1,37 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.master')
 
-<head>
-
-
-    <title>Wines &mdash; Website Template by Colorlib</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Pacifico&family=Poppins:wght@400;700&display=swap" rel="stylesheet">
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-    <link rel="stylesheet" href="{{ asset('fe-assets/css/owl.carousel.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('fe-assets/css/owl.theme.default.min.css') }}">
-
-
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-
-
-
-
-    <link rel="stylesheet" href="{{ asset('fe-assets/css/style.css')}}">
-
-
-    {{--
-
-
-
-  <link rel="stylesheet" href="{{ asset('fe-assets/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="css/jquery-ui.css">
-
-    <link rel="stylesheet" href="css/owl.theme.default.min.css">
-
-    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
-
-    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
-
-
-
-    <link rel="stylesheet" href="css/aos.css">
-    <link href="css/jquery.mb.YTPlayer.min.css" media="all" rel="stylesheet" type="text/css">
-    --}}
-
-</head>
-
-<body class="retro-neon-theme" data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
-
-    <div class="site-wrap">
-
-        <div class="site-mobile-menu site-navbar-target">
-            <div class="site-mobile-menu-header">
-                <div class="site-mobile-menu-close mt-3">
-                    <span class="icon-close2 js-menu-toggle"></span>
-                </div>
-            </div>
-            <div class="site-mobile-menu-body"></div>
-        </div>
-
-
-
-        <div class="header-top">
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-12 text-center">
-                        <a href="index.html" class="site-logo">
-                            <img src="images/logo.png" alt="Manang Wine" class="img-fluid">
-                        </a>
-                    </div>
-                    <a href="#" class="mx-auto d-inline-block d-lg-none site-menu-toggle js-menu-toggle text-black"><span
-                            class="icon-menu h3"></span></a>
-                </div>
-            </div>
-
-
-
-
-            <div class="site-navbar py-2 js-sticky-header site-navbar-target d-none pl-0 d-lg-block" role="banner">
-
-                <div class="container">
-                    <div class="d-flex align-items-center">
-
-                        <div class="mx-auto">
-                            <nav class="site-navigation position-relative text-left" role="navigation">
-                                <ul class="site-menu main-menu js-clone-nav mx-auto d-none pl-0 d-lg-block border-none">
-                                    <li class="active"><a href="index.html" class="nav-link text-left">Home</a></li>
-                                    <li><a href="about.html" class="nav-link text-left">About</a></li>
-                                    <li><a href="shop.html" class="nav-link text-left">Wines</a></li>
-                                    <li><a href="shop.html" class="nav-link text-left">Shop</a></li>
-                                    <li><a href="contact.html" class="nav-link text-left">Contact</a></li>
-                                </ul>
-                            </nav>
-
-                        </div>
-
-                    </div>
-                </div>
-
-            </div>
-
-        </div>
-
-
-        <div class="owl-carousel hero-slide owl-style">
-            <div class="intro-section container" style="background-image: url('/fe-assets/images/hero_1.jpg');">
-                <div class="row justify-content-center text-center align-items-center">
-                    <div class="col-md-8">
-                        <span class="sub-title">Royal Wine</span>
-                        <h1>Grape Wine</h1>
-                    </div>
-                </div>
-            </div>
-
-            <div class="intro-section container" style="background-image: url('/fe-assets/images/hero_2.jpg');">
-                <div class="row justify-content-center text-center align-items-center">
-                    <div class="col-md-8">
-                        <span class="sub-title">Welcome</span>
-                        <h1>Wines For Everyone</h1>
-                    </div>
-                </div>
-            </div>
-
-            <div class="intro-section container" style="background-image: url('/fe-assets/images/hero_3.jpg');">
-                <div class="row justify-content-center text-center align-items-center">
-                    <div class="col-md-8">
-                        <span class="sub-title">Discover</span>
-                        <h1>Fine Selections</h1>
-                    </div>
+@section('content')
+    <div class="owl-carousel hero-slide owl-style">
+        {{-- Hero Carousel items --}}
+        <div class="intro-section container" style="background-image: url('/fe-assets/images/hero_1.jpg');">
+            <div class="row justify-content-center text-center align-items-center">
+                <div class="col-md-8">
+                    <span class="sub-title">Royal Wine</span>
+                    <h1>Grape Wine</h1>
                 </div>
             </div>
         </div>
-
+        <div class="intro-section container" style="background-image: url('/fe-assets/images/hero_2.jpg');">
+            <div class="row justify-content-center text-center align-items-center">
+                <div class="col-md-8">
+                    <span class="sub-title">Welcome</span>
+                    <h1>Wines For Everyone</h1>
+                </div>
+            </div>
+        </div>
+        <div class="intro-section container" style="background-image: url('/fe-assets/images/hero_3.jpg');">
+            <div class="row justify-content-center text-center align-items-center">
+                <div class="col-md-8">
+                    <span class="sub-title">Discover</span>
+                    <h1>Fine Selections</h1>
+                </div>
+            </div>
+        </div>
     </div>
-
-
 
     <div class="site-section mt-5">
         <div class="container">
-
+            {{-- Products Section --}}
             <div class="row mb-5">
                 <div class="col-12 section-title text-center mb-5">
                     <h2 class="d-block">Our Products</h2>
@@ -148,22 +40,17 @@
                 </div>
             </div>
             <div class="row">
-
+                {{-- Product 1 --}}
                 <div class="col-lg-4 mb-5 col-md-6">
-
                     <div class="wine_v_1 text-center pb-4">
                         <a href="shop-single.html" class="thumbnail d-block mb-4"><img src="{{ asset('fe-assets/images/wine_2.png') }}" alt="Image" class="img-fluid"></a>
                         <div>
                             <h3 class="heading mb-1"><a href="#">Trius Cabernet France 2011</a></h3>
                             <span class="price">$629.00</span>
                         </div>
-
-
                         <div class="wine-actions">
-
                             <h3 class="heading-2"><a href="#">Trius Cabernet France 2011</a></h3>
                             <span class="price d-block">$629.00</span>
-
                             <div class="rating">
                                 <span class="fa fa-star"></span>
                                 <span class="fa fa-star"></span>
@@ -171,13 +58,11 @@
                                 <span class="fa fa-star"></span>
                                 <span class="fa fa-star-o"></span>
                             </div>
-
                             <a href="#" class="btn add"><span class="icon-shopping-bag mr-3"></span> Add to Cart</a>
                         </div>
                     </div>
-
                 </div>
-
+                {{-- Product 2 --}}
                 <div class="col-lg-4 mb-5 col-md-6">
                     <div class="wine_v_1 text-center pb-4">
                         <a href="shop-single.html" class="thumbnail d-block mb-4"><img src="{{ asset('fe-assets/images/wine_3.png') }}" alt="Image" class="img-fluid"></a>
@@ -185,13 +70,9 @@
                             <h3 class="heading mb-1"><a href="#">Trius Cabernet France 2011</a></h3>
                             <span class="price">$629.00</span>
                         </div>
-
-
                         <div class="wine-actions">
-
                             <h3 class="heading-2"><a href="#">Trius Cabernet France 2011</a></h3>
                             <span class="price d-block"><del>$900.00</del> $629.00</span>
-
                             <div class="rating">
                                 <span class="fa fa-star"></span>
                                 <span class="fa fa-star"></span>
@@ -199,12 +80,11 @@
                                 <span class="fa fa-star"></span>
                                 <span class="fa fa-star-o"></span>
                             </div>
-
                             <a href="#" class="btn add"><span class="icon-shopping-bag mr-3"></span> Add to Cart</a>
                         </div>
                     </div>
                 </div>
-
+                {{-- Product 3 --}}
                 <div class="col-lg-4 mb-5 col-md-6">
                     <div class="wine_v_1 text-center pb-4">
                         <a href="shop-single.html" class="thumbnail d-block mb-4"><img src="{{ asset('fe-assets/images/wine_1.png') }}" alt="Image" class="img-fluid"></a>
@@ -212,13 +92,9 @@
                             <h3 class="heading mb-1"><a href="#">Trius Cabernet France 2011</a></h3>
                             <span class="price">$629.00</span>
                         </div>
-
-
                         <div class="wine-actions">
-
                             <h3 class="heading-2"><a href="#">Trius Cabernet France 2011</a></h3>
                             <span class="price d-block"><del>$900.00</del> $629.00</span>
-
                             <div class="rating">
                                 <span class="fa fa-star"></span>
                                 <span class="fa fa-star"></span>
@@ -226,14 +102,10 @@
                                 <span class="fa fa-star"></span>
                                 <span class="fa fa-star-o"></span>
                             </div>
-
                             <a href="#" class="btn add"><span class="icon-shopping-bag mr-3"></span> Add to Cart</a>
                         </div>
                     </div>
                 </div>
-
-
-
             </div>
         </div>
     </div>
@@ -253,119 +125,65 @@
         <div class="container">
             <h2 class="text-center mb-5">What Our Customers Say</h2>
             <div class="owl-carousel owl-slide-3 owl-theme">
+                {{-- Testimonial 1 --}}
                 <blockquote class="testimony">
                     <img src="{{ asset('fe-assets/images/person_2.jpg') }}" alt="Collin Miller">
                     <p>&ldquo;The selection at this wine shop is unparalleled! Every bottle I've tried has been exquisite. Highly recommend for any wine enthusiast.&rdquo;</p>
                     <p class="small text-primary">&mdash; Collin Miller</p>
                 </blockquote>
+                {{-- Testimonial 2 --}}
                 <blockquote class="testimony">
                     <img src="{{ asset('fe-assets/images/person_1.jpg') }}" alt="Harley Perkins">
                     <p>&ldquo;Fantastic service and even better wines. They helped me pick the perfect bottle for a special occasion. I'll definitely be back!&rdquo;</p>
                     <p class="small text-primary">&mdash; Harley Perkins</p>
                 </blockquote>
+                {{-- Testimonial 3 --}}
                 <blockquote class="testimony">
                     <img src="{{ asset('fe-assets/images/person_2.jpg') }}" alt="Levi Morris">
                     <p>&ldquo;As a Laravel developer building scalable applications for global clients, I appreciate well-structured code. This shop's digital experience is as smooth as their Merlot!&rdquo;</p>
                     <p class="small text-primary">&mdash; Levi Morris</p>
                 </blockquote>
+                {{-- Testimonial 4 --}}
                 <blockquote class="testimony">
                     <img src="{{ asset('fe-assets/images/person_1.jpg') }}" alt="Allie Smith">
                     <p>&ldquo;Their online ordering process is seamless, and delivery was quick. The wines arrived perfectly packaged. A truly professional operation.&rdquo;</p>
                     <p class="small text-primary">&mdash; Allie Smith</p>
                 </blockquote>
-
             </div>
         </div>
     </div>
+@endsection
 
-
-
-
-
-   @include('components.footer')
-
-    </div>
-    <!-- .site-wrap -->
-
-
-    <!-- loader -->
-    {{--
-        <div id="loader" class="show fullscreen"><svg class="circular" width="48px" height="48px">
-            <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee" />
-            <circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#ff5e15" />
-        </svg></div>
-         --}}
-
-
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
-
-
-    {{--
-          <script src="js/jquery-3.3.1.min.js"></script>
-    <script src="js/jquery-migrate-3.0.1.min.js"></script>
-    <script src="js/jquery-ui.js"></script>
-    <script src="js/popper.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-  
-    <script src="js/jquery.stellar.min.js"></script>
-    <script src="js/jquery.countdown.min.js"></script>
-    <script src="js/bootstrap-datepicker.min.js"></script>
-    <script src="js/jquery.easing.1.3.js"></script>
-    <script src="js/aos.js"></script>
-    <script src="js/jquery.fancybox.min.js"></script>
-    <script src="js/jquery.sticky.js"></script>
-    <script src="js/jquery.mb.YTPlayer.min.js"></script>
-         --}}
-
-
-    <script src="{{ asset('fe-assets/js/owl.carousel.min.js') }}"></script>
-
-
-    <script src="{{ asset('fe-assets/js/main.js') }}"></script>
-
+@section('scripts')
     <script>
         $(document).ready(function() {
+            // Initialize Hero Section Carousel
             $(".owl-carousel.hero-slide").owlCarousel({
                 items: 1, // Display one item at a time
                 loop: true, // Enable looping
                 margin: 0, // No margin between items
-                nav: false, // Show navigation arrows
+                nav: false, // Hide navigation arrows for the hero section
                 dots: true, // Show navigation dots
                 autoplay: true, // Auto play carousel
                 autoplayTimeout: 5000, // Time between slides
                 smartSpeed: 800, // Speed of slide transition
-                // Add any other Owl Carousel options you need
+            });
+
+            // Initialize Testimonial Section Carousel
+            $(".owl-carousel.owl-slide-3").owlCarousel({
+                loop: true, // Enable continuous loop           
+                margin: 0, // Margin between items
+                nav: false, // Hide navigation arrows for testimonials
+                dots: true, // Show navigation dots
+                autoplay: true, // Autoplay the carousel
+                autoplayTimeout: 7000, // Time between slides
+                smartSpeed: 800, // Speed of the transition between slides
+                responsive: { // Responsive settings for different screen sizes
+                    0: { items: 1 },
+                    600: { items: 2 },
+                    992: { items: 3 }
+                }
             });
         });
-
-        // Initialize Testimonial Section Carousel
-        $(".owl-carousel.owl-slide-3").owlCarousel({
-            loop: true, // Enable continuous loop           
-            margin: 0, // Margin between items
-            nav: false, // Hide navigation arrows for testimonials (common design choice)
-            dots: true, // Show navigation dots
-            autoplay: true, // Autoplay the carousel
-            autoplayTimeout: 7000, // Time between slides in milliseconds (7 seconds)
-            smartSpeed: 800, // Speed of the transition between slides
-            responsive: { // Responsive settings for different screen sizes
-                0: {
-                    items: 1 // 1 item for screens smaller than 600px
-                },
-                600: {
-                    items: 2 // 2 items for screens 600px and up
-                },
-                992: {
-                    items: 3 // 3 items for screens 992px and up
-                }
-            }
-        });
     </script>
-
-
-
-
-</body>
-
-</html>
+@endsection
